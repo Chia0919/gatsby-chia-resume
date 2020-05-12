353667fe13312cdd4461a17212740609f5c05d79
chiapakmun <pakmunchia@ifca.com.my>
2020-05-12 13:32:56 +0800

    first commit

---
+++ gatsby-chia-resume/data/siteConfig.js
@@ -0,0 +1,276 @@
+module.exports = {
+  siteTitle: "Hi, I'm Chia Pak Mun!",
+  siteDescription: `This is my resume`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Chia Pak Mun',
+  twitterUsername: 'ajimenezdev',
+  githubUsername: 'ajimenezdev',
+  authorAvatar: '/images/chia_logo1.png',
+  authorDescription: `I'm a Software engineer and full-stack developer, currently focused in React development integrated with Graphql.<br/><br/>
+  I currently work remotely as a frontend developer and mainly working with <strong>React Hooks with Typescript, NodeJS and Graphql.</strong>`,
+  skills: [
+    {
+      name: 'React',
+      level: 85,
+    },
+    {
+      name: 'React Redux',
+      level: 75,
+    },
+    {
+      name: 'React Apollo',
+      level: 65,
+    },
+    {
+      name: 'HTML , CSS, Javascript',
+      level: 75,
+    },
+
+    {
+      name: 'NodeJs , Postgres , GraphQL',
+      level: 60,
+    },
+
+    {
+      name: 'Git',
+      level: 76,
+    },
+  ],
+  jobs: [
+    // {
+    //   company: 'Bitfinex',
+    //   begin: {
+    //     month: 'oct',
+    //     year: '2019',
+    //   },
+    //   duration: null,
+    //   location: 'Remote',
+    //   occupation: 'Mobile Engineer',
+    //   description:
+    //     'Working mainly with ReactNative as part of the Mobile development team, handling any task related to improve or fix existing features and developing new ones.',
+    // },
+    // {
+    //   company: 'Lifelabs.io',
+    //   begin: {
+    //     month: 'may',
+    //     year: '2018',
+    //   },
+    //   duration: '1 years and 5 months',
+    //   location: 'UK - Remote',
+    //   occupation: 'Lead Frontend Developer',
+    //   description:
+    //     'I joined the company as one of the main developers, leading the frontend and mobile app development and helping with the architecture and coordination of the backend and blokchain teams.',
+    // },
+    // {
+    //   company: 'Flipper',
+    //   begin: {
+    //     month: 'oct',
+    //     year: '2015',
+    //   },
+    //   duration: '2 years and 7 months',
+    //   location: 'London, UK',
+    //   occupation: 'Senior Developer',
+    //   description:
+    //     'I joined the company as one of the first 5 employees, I have been involved in all technical areas (backend, frontend, db, devops, bi) but with a higher responsibility on frontend, which I was leading inside the team',
+    // },
+    // {
+    //   company: 'Radisson Blu Edwardian London',
+    //   begin: {
+    //     month: 'oct',
+    //     year: '2013',
+    //   },
+    //   duration: '2 years',
+    //   location: 'London, UK',
+    //   occupation: 'Software developer',
+    //   description:
+    //     'Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool.',
+    // },
+    // {
+    //   company: 'HotelBeds',
+    //   begin: {
+    //     month: 'oct',
+    //     year: '2012',
+    //   },
+    //   duration: '1 year',
+    //   location: 'Majorca, Spain',
+    //   occupation: 'Software developer',
+    //   description:
+    //     'Member of a team in charge to create a new website to sell complementary holidays tickets and excursions, integrated inside the existing booking engine.',
+    // },
+    {
+      company: 'IFCA MSC BHD',
+      begin: {
+        month: 'jun',
+        year: '2019',
+      },
+      duration: null,
+      location: 'Petaling jaya, MY',
+      occupation: 'Software Engineer',
+      description:
+        'Working mainly with React hooks and integrated with GraphQL as part of the software development team, and helping team lead with the architecture and coordination of the front end development.',
+    },
+    {
+      company: 'Techvus (M) SDN BHD',
+      begin: {
+        month: 'Jan',
+        year: '2019',
+      },
+      duration: '4 months',
+      location: 'Petaling Jaya, MY',
+      occupation: 'Junior Software Engineer',
+      description: `Working with my senior in charge of customer Cryptocurrency exchange platform, creating new features and fixing some of the existing bugs. Member of another team in charge to create a new croptocurrency exchange website, translating designs and wire-frames into high quality code.`,
+    },
+    {
+      company: 'Techvus (M) SDN BHD',
+      begin: {
+        month: 'Sep',
+        year: '2018',
+      },
+      duration: '4 months',
+      location: 'Petaling Jaya, MY',
+      occupation: 'Intern developer',
+      description: `Working with my senior in charge of customer Cryptocurrency exchange platform, creating new features and fixing some of the existing bugs.`,
+    },
+  ],
+  publications: [
+    {
+      title: 'React Native intermediate level',
+      company_medium: 'OpenWebinars',
+      date: {
+        month: 'jun',
+        year: '2019',
+      },
+      link: 'https://openwebinars.net/cursos/react-native-intermedio/',
+      description:
+        'Online training recorded for spanish e-learning platform. The training is for developers who know a little bit of ReactNative and wants to learn more about the platform',
+    },
+    {
+      title: 'React Native for beginners',
+      company_medium: 'OpenWebinars',
+      date: {
+        month: 'jun',
+        year: '2019',
+      },
+      link: 'https://openwebinars.net/cursos/react-native-principiantes/',
+      description:
+        'Online training recorded for spanish e-learning platform. The training is for JS developers willing to learn the basics of ReactNative',
+    },
+  ],
+  education: [
+    {
+      school: 'KDU University College, Utropolis Glemarie, Shah Alam',
+      degree: 'Bachelor',
+      field: 'Software Engineering',
+      startYear: 2016,
+      endYear: 2019,
+      cgpa: '3.05/4.0',
+    },
+    {
+      school: 'KDU University College, Utropolis Glemarie, Shah Alam',
+      degree: 'Diploma',
+      field: 'in Computer Studies',
+      startYear: 2014,
+      endYear: 2016,
+      cgpa: '2.81/4.0',
+    },
+    {
+      school: 'SMK Tropicana',
+      degree: 'SPM',
+      field: '',
+      startYear: 2009,
+      endYear: 2013,
+    },
+  ],
+  languages: [
+    {
+      language: 'English',
+      level: 'Native tongue',
+      code: 'ES',
+    },
+    {
+      language: 'Chinese',
+      level: 'Native tongue',
+    },
+    {
+      language: 'Cantonese',
+      level: 'Fluid Written & Spoken',
+      code: 'GB',
+    },
+    {
+      language: 'Bahasa Melayu',
+      level: 'Fluid Written & Spoken',
+      code: 'GB',
+    },
+  ],
+  social: {
+    twitter: 'https://twitter.com/Chia09191',
+    linkedin: 'https://www.linkedin.com/in/chia-pak-mun-b4061a1a3/',
+    github: 'https://github.com/Chia0919',
+    email: 'chiapakmun0919@gmail.com',
+  },
+  hobbies: [
+    {
+      name: 'Gym',
+      icon: 'FaDumbbell',
+    },
+    {
+      name: 'Coding',
+      icon: 'FaCode',
+    },
+    // {
+    //   name: 'Hiking',
+    //   icon: 'FaHiking',
+    // },
+    // {
+    //   name: 'Traveling',
+    //   icon: 'FaPlane',
+    // },
+    // {
+    //   name: 'Movies & Series',
+    //   icon: 'FaTv',
+    // },
+  ],
+  siteUrl: 'https://alvarojimenezmartin.com',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/background.jpg',
+  googleAnalyticsId: 'UA-4492763-3',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/favicon.jpg',
+  // resume: '/resume_alvaro_jimenez.pdf',
+  headerLinks: [
+    // {
+    //   label: 'Alvaro Jimenez Martin',
+    //   url: '/',
+    // },
+    {
+      label: 'About',
+      url: 'about',
+    },
+    {
+      label: 'Skills',
+      url: 'skills',
+    },
+    {
+      label: 'Experience',
+      url: 'experience',
+    },
+    // {
+    //   label: 'Publications',
+    //   url: 'publications',
+    // },
+    {
+      label: 'Education',
+      url: 'education',
+    },
+    {
+      label: 'Interests',
+      url: 'interests',
+    },
+    // {
+    //   label: 'Contact',
+    //   url: 'contact',
+    // },
+  ],
+}
